e884248186ab0c9103a81a42975f1230785a15a8
lusia <panfluba@gmail.com>
2022-10-17 13:17:04 +0300

    feat: make 2 tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,10 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  const value = parseFloat(sampleActivity)
+  if (value < MODERN_ACTIVITY && value > 0 && typeof(sampleActivity) === 'string'){
+    return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
+  }
+  return false;
 }
+
 
 module.exports = {
   dateSample
