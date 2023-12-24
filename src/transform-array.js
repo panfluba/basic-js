dc6cc4fc95cdc9b9084c71526c0e745e4a588853
lusia <panfluba@gmail.com>
2023-12-24 03:25:55 +0300

    feat: finish tasks transform-array and vigenere-cipher

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!")
+  }
+
+  try {
+
+    let array = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === '--discard-next') {
+        if (arr[i + 1]) {
+          i = i + 2;
+        }
+      } else if (arr[i] === '--discard-prev') {
+        if (array.length !== 0) {
+          array.pop();
+        }
+      } else if (arr[i] === '--double-next') {
+        if (arr[i + 1]) {
+          array.push(arr[i + 1]);
+        }
+      } else if (arr[i] === '--double-prev') {
+        if (array.length !== 0) {
+          array.push(arr[i - 1]);
+        }
+      } else {
+        array.push(arr[i]);
+      }
+    }
+    return array;
+
+  } catch (e) {
+    throw new Error("Catch error")
+  }
 }
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direction = true) {
+    this.direct = direction
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(text, key) {
+    return this.executeVigenere(text, key, 'encrypt')
+  }
+
+  decrypt(text, key) {
+    return this.executeVigenere(text, key, 'decrypt')
+  }
+
+  executeVigenere(text, key, mode) {
+
+    if (!text || !key) {
+      throw new Error('Incorrect arguments!')
+    }
+
+    const ALPHABET_LENGTH = 26;
+    const isEncryptMode = mode === 'encrypt';
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let result = '';
+    let keyIndex = 0;
+    key = key.toUpperCase();
+    text = text.toUpperCase();
+
+    for (let i = 0; i < text.length; i++) {
+      if (alphabet.includes(text[i])) {
+        const textCharCode = text.charCodeAt(i) - 'A'.charCodeAt(0);
+        const keyCharCode = key.charCodeAt(keyIndex % key.length) - 'A'.charCodeAt(0);
+        keyIndex++;
+
+        let shiftedCharCode;
+        isEncryptMode
+            ? shiftedCharCode = (textCharCode + keyCharCode) % ALPHABET_LENGTH
+            : shiftedCharCode = (textCharCode - keyCharCode + ALPHABET_LENGTH) % ALPHABET_LENGTH;
+        result += String.fromCharCode(shiftedCharCode + 'A'.charCodeAt(0));
+      } else {
+        result += text[i];
+      }
+    }
+    return this.direct
+        ? result
+        : result.split('').reverse().join('');
   }
 }
 
